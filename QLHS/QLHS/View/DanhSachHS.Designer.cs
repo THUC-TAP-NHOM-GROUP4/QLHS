55be615eb8bd0e6c57617561b01589b8465e4ee6
doanthanhhuong <huongthanhlan@gmail.com>
2017-03-19 17:47:22 +0700

    Giao dien chinh

---
+++ QLHS/QLHS/View/DanhSachHS.Designer.cs
@@ -0,0 +1,276 @@
+﻿namespace QLHS.View
+{
+    partial class DanhSachHS
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DanhSachHS));
+            this.label1 = new System.Windows.Forms.Label();
+            this.dtgDanhSach = new System.Windows.Forms.DataGridView();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnTimKiem = new System.Windows.Forms.Button();
+            this.btnThoat = new System.Windows.Forms.Button();
+            this.btnLuu = new System.Windows.Forms.Button();
+            this.btnXoa = new System.Windows.Forms.Button();
+            this.btnSua = new System.Windows.Forms.Button();
+            this.btnThem = new System.Windows.Forms.Button();
+            this.btnXem = new System.Windows.Forms.Button();
+            this.columnMa = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.columnHoTen = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.columnLop = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.columnNgaySinh = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.columnGioiTinh = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.columnDanToc = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.columnTonGiao = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.columnDiaChi = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgDanhSach)).BeginInit();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.label1.Location = new System.Drawing.Point(290, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(164, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Danh Sách Học Sinh";
+            // 
+            // dtgDanhSach
+            // 
+            this.dtgDanhSach.BackgroundColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.dtgDanhSach.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgDanhSach.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.columnMa,
+            this.columnHoTen,
+            this.columnLop,
+            this.columnNgaySinh,
+            this.columnGioiTinh,
+            this.columnDanToc,
+            this.columnTonGiao,
+            this.columnDiaChi});
+            this.dtgDanhSach.Location = new System.Drawing.Point(2, 44);
+            this.dtgDanhSach.Name = "dtgDanhSach";
+            this.dtgDanhSach.Size = new System.Drawing.Size(774, 357);
+            this.dtgDanhSach.TabIndex = 1;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.label2);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 453);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(778, 30);
+            this.flowLayoutPanel1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label2.Location = new System.Drawing.Point(3, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(322, 16);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Copyright by NHOM 4-THUC TAP NHOM 2017";
+            // 
+            // btnTimKiem
+            // 
+            this.btnTimKiem.Image = ((System.Drawing.Image)(resources.GetObject("btnTimKiem.Image")));
+            this.btnTimKiem.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnTimKiem.Location = new System.Drawing.Point(539, 407);
+            this.btnTimKiem.Name = "btnTimKiem";
+            this.btnTimKiem.Size = new System.Drawing.Size(80, 40);
+            this.btnTimKiem.TabIndex = 30;
+            this.btnTimKiem.Text = "Tìm kiếm";
+            this.btnTimKiem.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnTimKiem.UseVisualStyleBackColor = true;
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.Image = ((System.Drawing.Image)(resources.GetObject("btnThoat.Image")));
+            this.btnThoat.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnThoat.Location = new System.Drawing.Point(696, 407);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(80, 40);
+            this.btnThoat.TabIndex = 29;
+            this.btnThoat.Text = "Thoát";
+            this.btnThoat.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnThoat.UseVisualStyleBackColor = true;
+            // 
+            // btnLuu
+            // 
+            this.btnLuu.Image = ((System.Drawing.Image)(resources.GetObject("btnLuu.Image")));
+            this.btnLuu.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnLuu.Location = new System.Drawing.Point(617, 407);
+            this.btnLuu.Name = "btnLuu";
+            this.btnLuu.Size = new System.Drawing.Size(80, 40);
+            this.btnLuu.TabIndex = 28;
+            this.btnLuu.Text = "Lưu";
+            this.btnLuu.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnLuu.UseVisualStyleBackColor = true;
+            // 
+            // btnXoa
+            // 
+            this.btnXoa.Image = ((System.Drawing.Image)(resources.GetObject("btnXoa.Image")));
+            this.btnXoa.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnXoa.Location = new System.Drawing.Point(460, 407);
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Size = new System.Drawing.Size(80, 40);
+            this.btnXoa.TabIndex = 27;
+            this.btnXoa.Text = "Xóa";
+            this.btnXoa.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnXoa.UseVisualStyleBackColor = true;
+            // 
+            // btnSua
+            // 
+            this.btnSua.Image = ((System.Drawing.Image)(resources.GetObject("btnSua.Image")));
+            this.btnSua.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnSua.Location = new System.Drawing.Point(381, 407);
+            this.btnSua.Name = "btnSua";
+            this.btnSua.Size = new System.Drawing.Size(80, 40);
+            this.btnSua.TabIndex = 26;
+            this.btnSua.Text = "Sửa";
+            this.btnSua.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnSua.UseVisualStyleBackColor = true;
+            // 
+            // btnThem
+            // 
+            this.btnThem.Image = ((System.Drawing.Image)(resources.GetObject("btnThem.Image")));
+            this.btnThem.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnThem.Location = new System.Drawing.Point(303, 407);
+            this.btnThem.Name = "btnThem";
+            this.btnThem.Size = new System.Drawing.Size(80, 40);
+            this.btnThem.TabIndex = 25;
+            this.btnThem.Text = "Thêm";
+            this.btnThem.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnThem.UseVisualStyleBackColor = true;
+            // 
+            // btnXem
+            // 
+            this.btnXem.Image = ((System.Drawing.Image)(resources.GetObject("btnXem.Image")));
+            this.btnXem.ImageAlign = System.Drawing.ContentAlignment.BottomLeft;
+            this.btnXem.Location = new System.Drawing.Point(224, 407);
+            this.btnXem.Name = "btnXem";
+            this.btnXem.Size = new System.Drawing.Size(80, 40);
+            this.btnXem.TabIndex = 24;
+            this.btnXem.Text = "Xem";
+            this.btnXem.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnXem.UseVisualStyleBackColor = true;
+            // 
+            // columnMa
+            // 
+            this.columnMa.HeaderText = "Mã Học Sinh";
+            this.columnMa.Name = "columnMa";
+            // 
+            // columnHoTen
+            // 
+            this.columnHoTen.HeaderText = "Họ và Tên";
+            this.columnHoTen.Name = "columnHoTen";
+            // 
+            // columnLop
+            // 
+            this.columnLop.HeaderText = "Lớp";
+            this.columnLop.Name = "columnLop";
+            // 
+            // columnNgaySinh
+            // 
+            this.columnNgaySinh.HeaderText = "Ngày Sinh";
+            this.columnNgaySinh.Name = "columnNgaySinh";
+            // 
+            // columnGioiTinh
+            // 
+            this.columnGioiTinh.HeaderText = "Giới Tính";
+            this.columnGioiTinh.Name = "columnGioiTinh";
+            // 
+            // columnDanToc
+            // 
+            this.columnDanToc.HeaderText = "Dân Tộc";
+            this.columnDanToc.Name = "columnDanToc";
+            // 
+            // columnTonGiao
+            // 
+            this.columnTonGiao.HeaderText = "Tôn Gíao";
+            this.columnTonGiao.Name = "columnTonGiao";
+            // 
+            // columnDiaChi
+            // 
+            this.columnDiaChi.HeaderText = "Địa Chỉ";
+            this.columnDiaChi.Name = "columnDiaChi";
+            // 
+            // DanhSachHS
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.ClientSize = new System.Drawing.Size(778, 483);
+            this.Controls.Add(this.btnTimKiem);
+            this.Controls.Add(this.btnThoat);
+            this.Controls.Add(this.btnLuu);
+            this.Controls.Add(this.btnXoa);
+            this.Controls.Add(this.btnSua);
+            this.Controls.Add(this.btnThem);
+            this.Controls.Add(this.btnXem);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.dtgDanhSach);
+            this.Controls.Add(this.label1);
+            this.Name = "DanhSachHS";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "DanhSachHS";
+            ((System.ComponentModel.ISupportInitialize)(this.dtgDanhSach)).EndInit();
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dtgDanhSach;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnTimKiem;
+        private System.Windows.Forms.Button btnThoat;
+        private System.Windows.Forms.Button btnLuu;
+        private System.Windows.Forms.Button btnXoa;
+        private System.Windows.Forms.Button btnSua;
+        private System.Windows.Forms.Button btnThem;
+        private System.Windows.Forms.Button btnXem;
+        private System.Windows.Forms.DataGridViewTextBoxColumn columnMa;
+        private System.Windows.Forms.DataGridViewTextBoxColumn columnHoTen;
+        private System.Windows.Forms.DataGridViewTextBoxColumn columnLop;
+        private System.Windows.Forms.DataGridViewTextBoxColumn columnNgaySinh;
+        private System.Windows.Forms.DataGridViewTextBoxColumn columnGioiTinh;
+        private System.Windows.Forms.DataGridViewTextBoxColumn columnDanToc;
+        private System.Windows.Forms.DataGridViewTextBoxColumn columnTonGiao;
+        private System.Windows.Forms.DataGridViewTextBoxColumn columnDiaChi;
+    }
+}
