efda5b41683481cfab4f6b7fbfa5ddd0f778927b
doanthanhhuong <huongthanhlan@gmail.com>
2017-03-25 23:09:40 +0700

    Ve lai form

---
+++ CSDL/TTN_QLHS.sql
@@ -0,0 +1,126 @@
+﻿USE [TTN_QLHS]
+GO
+
+-- TAO BANG BO MON
+CREATE TABLE Bomon
+(
+Ma VARCHAR(20) PRIMARY KEY NOT NULL,
+Ten VARCHAR(50) NOT NULL,
+Diadiem VARCHAR(200)
+)
+
+-- TAO BANG MON HOC
+CREATE TABLE Monhoc
+(
+Ma VARCHAR(20) PRIMARY KEY NOT NULL,
+Ten NVARCHAR(50) NOT NULL,
+Tinchi INT ,
+Sotiet INT,
+Gioithieu TEXT,
+Hinhthucthi NVARCHAR(20)
+)
+
+-- TAO BANG GIAO VIEN
+
+CREATE TABLE Giaovien
+(
+Ma VARCHAR(20) PRIMARY KEY,
+Ten NVARCHAR(50) NOT NULL,
+Gioitinh BIT,
+Ngaysinh DATE,
+Email VARCHAR(50),
+Anh VARCHAR(30),
+Luong MONEY,
+Nhiemvu NVARCHAR(30),
+Vaitro TEXT,
+Bomonma VARCHAR(20),
+CONSTRAINT fk_gv FOREIGN KEY(Bomonma) REFERENCES Bomon(Ma)
+)
+
+-- TAOJ BANGR LOP HOC PHAN
+CREATE TABLE Lophocphan
+(
+Ma VARCHAR(20) PRIMARY KEY NOT NULL,
+Ten NVARCHAR(50) NOT NULL,
+Hocki CHAR(5),
+NgayBD DATE,
+NgayKT DATE,
+Ngaythi DATE,
+Siso INT,
+Thu CHAR(10),
+Tiet INT ,
+Namhoc CHAR(5),
+Monma VARCHAR(20),
+Giaovienma VARCHAR(20),
+FOREIGN KEY(Monma) REFERENCES Monhoc(Ma),
+FOREIGN KEY(Giaovienma) REFERENCES Giaovien(Ma)
+)
+
+-- TAOJ BANG SINHVIEN
+CREATE TABLE Sinhvien
+(
+Ma INT PRIMARY KEY NOT NULL,
+Ten NVARCHAR(50) NOT NULL,
+Gioitinh BIT,
+Ngaysinh DATE,
+Email NVARCHAR(30),
+Doituong NVARCHAR(30),
+Sodienthoai NVARCHAR(30),
+Lopma VARCHAR(20),
+FOREIGN KEY (Lopma) REFERENCES Lophocphan(Ma)
+
+)
+
+-- TOA BANG DIEM
+CREATE TABLE Diem
+(
+Diem1 FLOAT ,
+Diem2 FLOAT,
+Diem3 FLOAT,
+Tongket FLOAT,
+Lopma VARCHAR(20),
+Sinhvienma VARCHAR(20),
+FOREIGN KEY(Lopma) REFERENCES Lophocphan(Ma),
+FOREIGN KEY(Sinhvienma) REFERENCES Sinhvien(Ma)
+)
+
+-- INSERT INTO
+ 
+
+ -- BANG BOMON
+ INSERT INTO Bomon VALUES('BM001',N'Công nghệ thông tin',N'Tầng 19 nhà S1'),
+                         ('BM002',N'Công nghệ thông tin',N'Tầng 19 nhà S1'),
+				         ('BM003',N'Công nghệ thông tin',N'Tầng 19 nhà S1'),
+				         ('BM004',N'Công nghệ thông tin',N'Tầng 19 nhà S1')
+
+      -- BANG MON HOC
+	  INSERT INTO Monhoc VALUES('MH001',N'Công nghệ Web','3','75',N'Công nghệ Web',N'Vấn đáp'),
+	                           ('MH002',N'Công nghệ phần mềm','3','75',N'Công nghệ phần mềm',N'Vấn đáp'),
+							   ('MH003',N'Xây dựng và thiết kế','3','75',N'Xây dựng và thiết kế',N'Vấn đáp'),
+							   ('MH004',N'Đường lối quân sự Đảng','3','75',N'Đường lối quân sự Đảng',N'Vấn đáp')
+
+
+		-- BANG GIAO VIEN
+		INSERT INTO Giaovien VALUES('GV001',N'Nguyen Van A',N'1','4/3/1987','NVA@gmail.com','NVA1234','123456','anh1','120',N'giảng dạy','012345679','aasg',N'thạc sĩ',N'Từ liêm - Hà Nội',N'giáo viên','BM001'),
+		                           ('GV002',N'Nguyen Van B',N'1','4/3/1987','NVA@gmail.com','NVA1234','123456','anh1','120',N'giảng dạy','012345679','aasg',N'thạc sĩ',N'Từ liêm - Hà Nội',N'giáo viên','BM002'),
+								   ('GV003',N'Nguyen Van C',N'1','4/3/1987','NVA@gmail.com','NVA1234','123456','anh1','120',N'giảng dạy','012345679','aasg',N'thạc sĩ',N'Từ liêm - Hà Nội',N'giáo viên','BM003'),
+								   ('GV004',N'Nguyen Van D',N'1','4/3/1987','NVA@gmail.com','NVA1234','123456','anh1','120',N'giảng dạy','012345679','aasg',N'thạc sĩ',N'Từ liêm - Hà Nội',N'giáo viên','BM004')
+
+		-- BANG LỚP HỌC PHẦN
+		INSERT INTO Lophocphan VALUES('L001',N'Nguyễn Văn A','1','8/15/2016','6/1/2017','5/3/2017','34','2016-2017','3','4-6','MH001','GV001'),
+		                             ('L002',N'Nguyễn Văn B','1','8/15/2016','6/1/2017','5/3/2017','34','2016-2017','3','4-6','MH001','GV002'),
+									 ('L003',N'Nguyễn Văn C','1','8/15/2016','6/1/2017','5/3/2017','34','2016-2017','3','4-6','MH001','GV003'),
+									 ('L004',N'Nguyễn Văn D','1','8/15/2016','6/1/2017','5/3/2017','34','2016-2017','3','4-6','MH001','GV004')
+
+		-- BANG SINH VIÊN
+		INSERT INTO Sinhvien VALUES('SV001',N'Nguyễn Văn A','1','4/3/1997','NVA@gmail.com','Sinh viên','012345322','L001'),
+		                           ('SV002',N'Nguyễn Văn B','1','4/3/1997','NVA@gmail.com','Sinh viên','012345322','L002'),
+								   ('SV003',N'Nguyễn Văn C','1','4/3/1997','NVA@gmail.com','Sinh viên','012345322','L003'),
+								   ('SV004',N'Nguyễn Văn D','1','4/3/1997','NVA@gmail.com','Sinh viên','012345322','L004')
+
+
+		-- BẢNG ĐIỂM
+		INSERT INTO Diem VALUES('7','8','9','24','L001','SV001'),
+		                       ('7','8','9','24','L001','SV002'),
+							   ('7','8','9','24','L001','SV003'),
+							   ('7','8','9','24','L002','SV001')
---
+++ QuanLyHocSinh/View/AddHocSinh.Designer.cs
@@ -0,0 +1,271 @@
+﻿namespace QuanLyHocSinh
+{
+    partial class AddHocSinh
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AddHocSinh));
+            this.btnThoat = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txtLop = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.dtpNgaySinh = new System.Windows.Forms.DateTimePicker();
+            this.txtDiaChi = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtDanToc = new System.Windows.Forms.TextBox();
+            this.txtGioiTinh = new System.Windows.Forms.TextBox();
+            this.txtHoTenHS = new System.Windows.Forms.TextBox();
+            this.txtMaHS = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.Location = new System.Drawing.Point(239, 417);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(75, 21);
+            this.btnThoat.TabIndex = 87;
+            this.btnThoat.Text = "Thoát";
+            this.btnThoat.UseVisualStyleBackColor = true;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(96, 417);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 21);
+            this.btnAdd.TabIndex = 86;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Image = ((System.Drawing.Image)(resources.GetObject("label9.Image")));
+            this.label9.Location = new System.Drawing.Point(40, 32);
+            this.label9.MinimumSize = new System.Drawing.Size(60, 28);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(60, 28);
+            this.label9.TabIndex = 85;
+            this.label9.Text = "                 ";
+            this.label9.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            // 
+            // txtLop
+            // 
+            this.txtLop.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.txtLop.Location = new System.Drawing.Point(143, 163);
+            this.txtLop.Name = "txtLop";
+            this.txtLop.Size = new System.Drawing.Size(186, 22);
+            this.txtLop.TabIndex = 84;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label6.Location = new System.Drawing.Point(37, 163);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 16);
+            this.label6.TabIndex = 83;
+            this.label6.Text = "Lớp:";
+            // 
+            // dtpNgaySinh
+            // 
+            this.dtpNgaySinh.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.dtpNgaySinh.Location = new System.Drawing.Point(143, 205);
+            this.dtpNgaySinh.Name = "dtpNgaySinh";
+            this.dtpNgaySinh.Size = new System.Drawing.Size(186, 22);
+            this.dtpNgaySinh.TabIndex = 82;
+            // 
+            // txtDiaChi
+            // 
+            this.txtDiaChi.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.txtDiaChi.Location = new System.Drawing.Point(143, 340);
+            this.txtDiaChi.Name = "txtDiaChi";
+            this.txtDiaChi.Size = new System.Drawing.Size(186, 22);
+            this.txtDiaChi.TabIndex = 81;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label8.Location = new System.Drawing.Point(33, 347);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(54, 16);
+            this.label8.TabIndex = 80;
+            this.label8.Text = "Địa Chỉ:";
+            // 
+            // txtDanToc
+            // 
+            this.txtDanToc.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.txtDanToc.Location = new System.Drawing.Point(143, 292);
+            this.txtDanToc.Name = "txtDanToc";
+            this.txtDanToc.Size = new System.Drawing.Size(186, 22);
+            this.txtDanToc.TabIndex = 79;
+            // 
+            // txtGioiTinh
+            // 
+            this.txtGioiTinh.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.txtGioiTinh.Location = new System.Drawing.Point(143, 249);
+            this.txtGioiTinh.Name = "txtGioiTinh";
+            this.txtGioiTinh.Size = new System.Drawing.Size(186, 22);
+            this.txtGioiTinh.TabIndex = 78;
+            // 
+            // txtHoTenHS
+            // 
+            this.txtHoTenHS.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.txtHoTenHS.Location = new System.Drawing.Point(143, 121);
+            this.txtHoTenHS.Name = "txtHoTenHS";
+            this.txtHoTenHS.Size = new System.Drawing.Size(186, 22);
+            this.txtHoTenHS.TabIndex = 77;
+            // 
+            // txtMaHS
+            // 
+            this.txtMaHS.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.txtMaHS.Location = new System.Drawing.Point(143, 82);
+            this.txtMaHS.Name = "txtMaHS";
+            this.txtMaHS.Size = new System.Drawing.Size(186, 22);
+            this.txtMaHS.TabIndex = 76;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label7.Location = new System.Drawing.Point(34, 298);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(60, 16);
+            this.label7.TabIndex = 75;
+            this.label7.Text = "Dân Tộc:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label5.Location = new System.Drawing.Point(34, 249);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(65, 16);
+            this.label5.TabIndex = 74;
+            this.label5.Text = "Giới Tính:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label4.Location = new System.Drawing.Point(34, 205);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(72, 16);
+            this.label4.TabIndex = 73;
+            this.label4.Text = "Ngày Sinh:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label3.Location = new System.Drawing.Point(34, 121);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 16);
+            this.label3.TabIndex = 72;
+            this.label3.Text = "Họ Tên:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label2.Location = new System.Drawing.Point(34, 88);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 16);
+            this.label2.TabIndex = 71;
+            this.label2.Text = "Mã học sinh:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label1.Location = new System.Drawing.Point(116, 41);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(197, 18);
+            this.label1.TabIndex = 70;
+            this.label1.Text = "Thêm Thông tin sinh viên :";
+            // 
+            // AddHocSinh
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(362, 471);
+            this.Controls.Add(this.btnThoat);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.txtLop);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.dtpNgaySinh);
+            this.Controls.Add(this.txtDiaChi);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.txtDanToc);
+            this.Controls.Add(this.txtGioiTinh);
+            this.Controls.Add(this.txtHoTenHS);
+            this.Controls.Add(this.txtMaHS);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "AddHocSinh";
+            this.Text = "AddHocSinh";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnThoat;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txtLop;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.DateTimePicker dtpNgaySinh;
+        private System.Windows.Forms.TextBox txtDiaChi;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtDanToc;
+        private System.Windows.Forms.TextBox txtGioiTinh;
+        private System.Windows.Forms.TextBox txtHoTenHS;
+        private System.Windows.Forms.TextBox txtMaHS;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
